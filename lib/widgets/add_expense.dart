10a25f830d8ab272a1fad8c80b57870be47bfc38
ZeeshanShabbir <zeeshan.shabbir79@gmail.com>
2025-08-21 07:56:57 +0200

    Add new bottom sheet

--- lib/widgets/add_expense.dart
+++ lib/widgets/add_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -10,19 +11,89 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
+  final _titleController = TextEditingController();
+  final _costController = TextEditingController();
+  DateTime? selectedDate;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _costController.dispose();
+    super.dispose();
+  }
+
+  void _showDatePickerMondal() async {
+    final lastDate = DateTime.now();
+    final firstDate = DateTime(lastDate.year - 1, lastDate.month, lastDate.day);
+    var userSelectedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: lastDate,
+    );
+    setState(() {
+      selectedDate = userSelectedDate;
+    });
+  }
+
+  final formatter = DateFormat.yMd();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(8),
+      padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(label: Text("Title")),
           ),
-          TextField(
-            maxLength: 50,
-            decoration: InputDecoration(label: Text("Cost")),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _costController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 6,
+                  decoration: InputDecoration(label: Text("Cost")),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      selectedDate == null
+                          ? "Select Date"
+                          : formatter.format(selectedDate!),
+                    ),
+                    SizedBox(width: 8),
+                    IconButton(
+                      onPressed: _showDatePickerMondal,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Clear"),
+              ),
+              Spacer(),
+              OutlinedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_costController.text);
+                },
+                child: Text("Save Expense"),
+              ),
+            ],
           ),
         ],
       ),
